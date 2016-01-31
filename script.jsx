81067ea6407774627ce0732913e2e08868b28843
Irene Robb <IreneRobb@Irenes-MacBook-Pro.local>
2016-01-31 20:14:07 +1300

    adding cards

--- script.jsx
+++ script.jsx
@@ -3,43 +3,34 @@ import ReactDOM from 'react-dom'
 
 const mountNode = document.getElementById('app')
 
-class Button extends Component {
-  localHandleClick() {
-    this.props.localHandleClick(this.props.increment)
+class Card extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: ("https://api.github.com/" + this.props.login)
+    }
+  }
+  componentWillMount() {
+    const { data } = this.state
+    this.setState(data)
   }
   render() {
     return (
-      <button onClick={this.props.localHandleClick.bind(this)}>+{this.props.increment}</button>
-    )
-  }
-}
-
-class Result extends Component {
-  render() {
-    return (
-      <div>{this.props.localCounter}</div>
+      <div>
+        <img src="{this.state.avatar_url}" width="80" />
+        <h3>{this.state.name}</h3>
+        <hr/>
+      </div>
     )
   }
 }
 
 class Main extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      counter: 0
-    }
-  }
-  handleClick(increment) {
-    this.setState({counter: this.state.counter+increment})
-  }
   render() {
     return (
       <div>
-        <Button localHandleClick={this.handleClick} increment={1} />
-        <Button localHandleClick={this.handleClick} increment={5} />
-        <Button localHandleClick={this.handleClick} increment={10} />
-        <Button localHandleClick={this.handleClick} increment={100} />
-        <Result localCounter={this.state.counter} />
+        <Card login="spicyj"/>
+        <Card login="petehunt"/>
       </div>
     )
   }
